cfdb9eeadd6dabddf8fc5194690079c457cb5aa9
Sven Bohm <bohms@msu.edu>
2015-03-11 12:28:07 -0400

    logging to browser over the websocket

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
 	"github.com/gorilla/mux"
 	"github.com/gorilla/websocket"
 	zmq "github.com/pebbe/zmq4"
@@ -18,7 +18,8 @@ func (c *connection) reader() {
 	for message := range c.send {
 		err := c.ws.WriteMessage(websocket.TextMessage, message)
 		if err != nil {
-			log.Fatal(err)
+			log.Println(err)
+			return
 		}
 	}
 	c.ws.Close()
@@ -32,6 +33,7 @@ type qcl struct {
 func (q *qcl) read() {
 	socket, err := zmq.NewSocket(zmq.SUB)
 	if err != nil {
+		log.Println("ZMQ socket error")
 		log.Fatal(err)
 	}
 	defer socket.Close()
@@ -41,27 +43,38 @@ func (q *qcl) read() {
 	for {
 		data, err := socket.Recv(0)
 		if err != nil {
-			log.Fatal(err)
+			log.Println("line 44")
+			log.Println(err)
+			return
 		}
 		q.send <- []byte(data)
 	}
 }
 
-func QclHandler(w http.ResponseWriter, r *http.Request) {
-	// data := QclReader()
-	// fmt.Println(data)
-	// w.Write(data)
+var upgrader = websocket.Upgrader{ReadBufferSize: 1024, WriteBufferSize: 1024}
+
+func QclHandler(q *qcl, w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	c := &connection{send: q.send, ws: ws}
+	c.reader()
 }
 
 func main() {
 
 	instrument := &qcl{send: make(chan []byte, 512)}
 	go instrument.read()
-	for {
-		fmt.Println(string(<-instrument.send))
-	}
+	// for {
+	// 	fmt.Println(string(<-instrument.send))
+	// }
 	r := mux.NewRouter()
-	r.HandleFunc("/", QclHandler)
+	r.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		QclHandler(instrument, w, r)
+	})
+	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./public/")))
 	http.Handle("/", r)
 	http.ListenAndServe(":9000", nil)
 }
