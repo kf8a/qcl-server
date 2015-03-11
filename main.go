d4b6dc105c1cf4b6a4e89e74ba920869e3930fb7
Sven Bohm <bohms@msu.edu>
2015-03-10 21:06:36 -0400

    basic reading from the zmq socket by a go routine

--- main.go
+++ main.go
@@ -1,24 +1,67 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
 	zmq "github.com/pebbe/zmq4"
+	"log"
+	"net/http"
 )
 
-func QclHandler(w http.ResponseWriter, r *http.Request) {
+type connection struct {
+	ws   *websocket.Conn
+	send chan []byte
+}
+
+func (c *connection) reader() {
+	for message := range c.send {
+		err := c.ws.WriteMessage(websocket.TextMessage, message)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	c.ws.Close()
+}
+
+type qcl struct {
+	socket zmq.Socket
+	send   chan []byte
+}
+
+func (q *qcl) read() {
 	socket, err := zmq.NewSocket(zmq.SUB)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer socket.Close()
-	socket.Bind("tcp://192.108.190.96:5550")
-	w.Write(socket.Read())
+
+	socket.SetSubscribe("")
+	socket.Connect("tcp://192.108.190.96:5550")
+	for {
+		data, err := socket.Recv(0)
+		if err != nil {
+			log.Fatal(err)
+		}
+		q.send <- []byte(data)
+	}
+}
+
+func QclHandler(w http.ResponseWriter, r *http.Request) {
+	// data := QclReader()
+	// fmt.Println(data)
+	// w.Write(data)
 }
 
 func main() {
+
+	instrument := &qcl{send: make(chan []byte, 512)}
+	go instrument.read()
+	for {
+		fmt.Println(string(<-instrument.send))
+	}
 	r := mux.NewRouter()
 	r.HandleFunc("/", QclHandler)
 	http.Handle("/", r)
+	http.ListenAndServe(":9000", nil)
 }
